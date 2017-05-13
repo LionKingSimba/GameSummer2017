fe7feb9a7167f9b78fdc3c329c4209ef0f7acb80
Sam Wu <sjwu@ualberta.ca>
2017-05-12 17:30:24 -0700

    core classes

---
+++ Sam's World/Assets/Scripts/Classes/BaseMageClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BaseMageClass : BaseClass {
+
+    public void MageClass() {
+        ClassName = "Mage";
+        ClassDescription = "Mages use their spells and mana to battle enemies.";
+        Strength = 5;
+        Dexterity = 0;
+        Intelligence = 10;
+        Endurance = 5;
+        Agility = 0;
+        Wisdom = 10;
+    }
+
+}
---
+++ Sam's World/Assets/Scripts/Classes/BaseRogueClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BaseRogueClass : BaseClass {
+
+    public void RogueClass() {
+        ClassName = "Rogue";
+        ClassDescription = "Rogues use their finesse and speed to battle enemies.";
+        Strength = 5;
+        Dexterity = 10;
+        Intelligence = 0;
+        Endurance = 5;
+        Agility = 10;
+        Wisdom = 0;
+    }
+
+}
