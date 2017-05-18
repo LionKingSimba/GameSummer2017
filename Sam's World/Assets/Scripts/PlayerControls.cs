4121ddae7bc22ce194550a30f4532cb06d880a24
Sam Wu <sjwu@ualberta.ca>
2017-05-17 21:23:10 -0700

    player movement animation using transitions

--- Sam's World/Assets/Scripts/PlayerControls.cs
+++ Sam's World/Assets/Scripts/PlayerControls.cs
@@ -7,13 +7,15 @@ public class PlayerControls : MonoBehaviour
 
     public float MoveSpeed;
 
-	// Use this for initialization
+    private Animator anime;
+
+	// initialization
 	void Start ()
     {
-	        	
+        anime = GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
+	// Update called once per frame
 	void Update ()
     {
 	    if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
@@ -25,6 +27,9 @@ public class PlayerControls : MonoBehaviour
         {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * MoveSpeed * Time.deltaTime, 0f));
         }
+
+        anime.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anime.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
     }
 
 }
