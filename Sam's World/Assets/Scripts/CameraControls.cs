a28b82679e8ae5727a06b957f2672b40d1a473a3
Sam Wu <sjwu@ualberta.ca>
2017-05-19 21:31:45 -0700

    camera tracking and tile snapping

---
+++ Sam's World/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour {
+
+    public GameObject FollowTarget;
+    public float CameraSpeed;
+
+    private Vector3 targetposition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targetposition = new Vector3(FollowTarget.transform.position.x, FollowTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetposition, CameraSpeed * Time.deltaTime);
+	}
+}
