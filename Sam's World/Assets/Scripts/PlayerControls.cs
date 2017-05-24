78ef1a978529b82615c954c4b437c5fc44aeb7e0
Sam Wu <sjwu@ualberta.ca>
2017-05-23 21:11:28 -0700

    collisions based on rigid body physics -ice skating mode

--- Sam's World/Assets/Scripts/PlayerControls.cs
+++ Sam's World/Assets/Scripts/PlayerControls.cs
@@ -8,6 +8,8 @@ public class PlayerControls : MonoBehaviour
     public float MoveSpeed;
 
     private Animator player;
+    private Rigidbody2D playerrigidbody;
+
     private bool playermoving;
     private Vector2 lastmove;
 
@@ -15,6 +17,7 @@ public class PlayerControls : MonoBehaviour
 	void Start ()
     {
         player = GetComponent<Animator>();
+        playerrigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update called once per frame
@@ -24,14 +27,16 @@ public class PlayerControls : MonoBehaviour
 
 	    if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * MoveSpeed * Time.deltaTime, 0f, 0f));
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * MoveSpeed * Time.deltaTime, 0f, 0f));
+            playerrigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * MoveSpeed, playerrigidbody.velocity.y);
             playermoving = true;
             lastmove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * MoveSpeed * Time.deltaTime, 0f));
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * MoveSpeed * Time.deltaTime, 0f));
+            playerrigidbody.velocity = new Vector2(playerrigidbody.velocity.x, Input.GetAxisRaw("Vertical") * MoveSpeed);
             playermoving = true;
             lastmove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
