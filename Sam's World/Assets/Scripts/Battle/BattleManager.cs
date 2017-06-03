0f5d665cc2a45b5f2f0a2de93a82ae8cd377861a
LionKingSimba <ttran1@ualberta.ca>
2017-06-03 14:35:34 -0600

    BattleManager script

---
+++ Sam's World/Assets/Scripts/Battle/BattleManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour {
+
+    GameObject player;
+    GameObject[] allies;
+    GameObject ally1;
+    GameObject ally2;
+
+    GameObject[] enemies;
+
+	// Use this for initialization
+	void Start () {
+
+		// Get the player, ally and enemy GameObjects.
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        if (allies == null)
+        {
+            allies = GameObject.FindGameObjectsWithTag("Ally");
+            if (allies.Length > 0)
+            {
+                if (ally1 == null)
+                {
+                    ally1 = allies[0];
+                }
+                if (ally2 == null && allies.Length > 1)
+                {
+                    ally2 = allies[1];
+                }
+            }
+        }
+
+        Debug.Log("The Allies are: ");
+        foreach (GameObject ally in allies)
+        {
+            Debug.Log(ally);
+        }
+
+        if (enemies == null)
+        {
+            enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        }
+
+        Debug.Log("The Enemies are: ");
+        foreach (GameObject enemy in enemies)
+        {
+            Debug.Log(enemy);
+        }
+
+        // Display ally health and mana.
+        displayStatus(player);
+        foreach (GameObject ally in allies)
+        {
+            displayStatus(ally);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void displayStatus (GameObject character)
+    /* Displays a character's health and mana.
+     * 
+     * Parameters:
+     *      character: the character whose health and mana to display
+     */
+    {
+        CharacterStatus status = character.GetComponent<CharacterStatus>();
+        int health = status.health;
+        int mana = status.mana;
+
+        Debug.Log("Health: " + health);
+        Debug.Log("Mana: " + mana);
+    }
+
+    
+}
---
+++ Sam's World/Assets/Scripts/Battle/CharacterStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStatus : MonoBehaviour {
+
+    /* This script handles the character's stats (health, attack, mana, etc.)  as well as equipment. 
+     Can be for both enemies and allies. */
+
+    public int health;
+    public int mana;
+
+    public int strength;
+    public int intelligence;
+    public int dexterity;
+    public int luck;
+    public int speed;
+
+    public int defense;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
