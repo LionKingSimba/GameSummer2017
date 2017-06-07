bddd2d2572e084aeeeb350fa647045e9f7fd26e4
Sam Wu <sjwu@ualberta.ca>
2017-06-06 18:54:49 -0700

    fixed diagonal movement

--- Sam's World/Assets/Scripts/PlayerControls.cs
+++ Sam's World/Assets/Scripts/PlayerControls.cs
@@ -6,6 +6,9 @@ public class PlayerControls : MonoBehaviour
 {
 
     public float MoveSpeed;
+    public float DiagonalMoveModifier;
+
+    private float currentmovespeed;
 
     private Animator player;
     private Rigidbody2D playerrigidbody;
@@ -42,30 +45,44 @@ public class PlayerControls : MonoBehaviour
     {
         playermoving = false;
 
+        //horizontal movement
 	    if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
             //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * MoveSpeed * Time.deltaTime, 0f, 0f));
-            playerrigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * MoveSpeed, playerrigidbody.velocity.y);
+            playerrigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * currentmovespeed, playerrigidbody.velocity.y);
             playermoving = true;
             LastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
+        //vertical movement
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
             //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * MoveSpeed * Time.deltaTime, 0f));
-            playerrigidbody.velocity = new Vector2(playerrigidbody.velocity.x, Input.GetAxisRaw("Vertical") * MoveSpeed);
+            playerrigidbody.velocity = new Vector2(playerrigidbody.velocity.x, Input.GetAxisRaw("Vertical") * currentmovespeed);
             playermoving = true;
             LastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
 
+        //stop horizontal movement
         if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
         {
             playerrigidbody.velocity = new Vector2(0f, playerrigidbody.velocity.y);
         }
 
+        //stop vertical movement
         if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
         {
             playerrigidbody.velocity = new Vector2(playerrigidbody.velocity.x, 0f);
+        }
+
+        //diagonal movespeed
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
+        {
+            currentmovespeed = MoveSpeed * DiagonalMoveModifier;
+        }
+        else
+        {
+            currentmovespeed = MoveSpeed;
         }
 
         player.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
