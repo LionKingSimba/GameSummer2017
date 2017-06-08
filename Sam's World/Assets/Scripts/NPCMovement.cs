fbd308016b01c09539991fb5bc6e0c51baa03eec
Sam Wu <sjwu@ualberta.ca>
2017-06-07 20:36:55 -0700

    add npc villager char

---
+++ Sam's World/Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour {
+
+    public float MoveSpeed;
+
+    private Rigidbody2D NPCrigidbody;
+
+    public bool IsMoving;
+    public float MoveTime;
+    public float WaitTime;
+
+    private float movecounter;
+    private float waitcounter;
+
+    private int Direction;
+
+	// Use this for initialization
+	void Start () {
+		NPCrigidbody = GetComponent<Rigidbody2D>();
+
+        movecounter = MoveTime;
+        waitcounter = WaitTime;
+
+        ChooseDirection();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(IsMoving)
+        {
+            movecounter -= Time.deltaTime; //move for MoveTime seconds
+       
+            //move in chosen direction
+            switch(Direction)
+            {
+                case 0:
+                    NPCrigidbody.velocity = new Vector2(0, MoveSpeed);
+                    break;
+
+                case 1:
+                    NPCrigidbody.velocity = new Vector2(MoveSpeed, 0);
+                    break;
+
+                case 2:
+                    NPCrigidbody.velocity = new Vector2(0, -MoveSpeed);
+                    break;
+
+                case 3:
+                    NPCrigidbody.velocity = new Vector2(-MoveSpeed, 0);
+                    break;
+            }
+
+            //stop moving if no moves left
+            if (movecounter < 0)
+            {
+                IsMoving = false;
+                waitcounter = WaitTime;
+            }
+        }
+        else
+        {
+            waitcounter -= Time.deltaTime;
+
+            NPCrigidbody.velocity = Vector2.zero;
+
+            if(waitcounter < 0)
+            {
+                ChooseDirection();
+            }
+        }
+	}
+
+    //use random int for direction
+    //0=up, 1=right, 2=down, 3=left
+    public void ChooseDirection()
+    {
+        Direction = Random.Range(0, 4);
+        IsMoving = true;
+        movecounter = MoveTime;
+    }
+
+}
