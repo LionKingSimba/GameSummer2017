47065f4405d00288fbe5df3ab3689933af562c54
Sam Wu <sjwu@ualberta.ca>
2017-06-08 19:59:32 -0700

    dialogue with npc works

---
+++ Sam's World/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public string dialogue;
+
+    private DialogueControls dialoguecontroller;
+
+	// Use this for initialization
+	void Start () {
+        dialoguecontroller = FindObjectOfType<DialogueControls>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D boxcolliderobj)
+    {
+        if(boxcolliderobj.gameObject.name == "Player")
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                dialoguecontroller.ShowBox(dialogue);
+            }
+        }
+    }
+
+}
