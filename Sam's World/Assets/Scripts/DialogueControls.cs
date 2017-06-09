d42889c755ca772d90d1f5b3dc4197dee2128c19
Sam Wu <sjwu@ualberta.ca>
2017-06-08 19:30:04 -0700

    added text box

---
+++ Sam's World/Assets/Scripts/DialogueControls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControls : MonoBehaviour {
+
+    public GameObject TextBox;
+    public Text Text;
+
+    public bool DialogBoxOn;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(DialogBoxOn && Input.GetKeyDown(KeyCode.Space))
+        {
+            TextBox.SetActive(false);
+            DialogBoxOn = false;
+        }
+	}
+
+    public void ShowBox(string dialogue)
+    {
+        DialogBoxOn = true;
+        TextBox.SetActive(true);
+        Text.text = dialogue;
+    }
+
+}
---
+++ Sam's World/Assets/Scripts/GUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUI : MonoBehaviour {
+
+    private static bool guiinscene;
+
+	// Use this for initialization
+	void Start () {
+        //if new scene has no gui, keep the past scene gui
+        if (!guiinscene)
+        {
+            guiinscene = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        //if new scene already has gui, destroy the latest (duplicate) gui
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
