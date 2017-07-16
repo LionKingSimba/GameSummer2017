181dd4b0dde6994e69dc8500ecf909ed00acdc03
Sam Wu <sjwu@ualberta.ca>
2017-07-15 23:01:05 -0700

    drag ui

--- Sam's World/Assets/Scripts/ItemData.cs
+++ Sam's World/Assets/Scripts/ItemData.cs
@@ -1,11 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemData : MonoBehaviour
+using UnityEngine.EventSystems; //for inventory ui
+
+public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
     public Item item;
     public int amount;
 
+    //interface implementation for IBeginDragHandler
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    //interface implementation for IDragHandler
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    //interface implementation for IEndDragHandler
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
 }
