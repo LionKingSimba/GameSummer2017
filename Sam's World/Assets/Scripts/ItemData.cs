8ab79fca6b2e53ae328f5ecfc13ff3e1f3c9b54b
Sam Wu <sjwu@ualberta.ca>
2017-07-15 23:10:40 -0700

    implement enddrag

--- Sam's World/Assets/Scripts/ItemData.cs
+++ Sam's World/Assets/Scripts/ItemData.cs
@@ -11,13 +11,17 @@ public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
     public Item item;
     public int amount;
 
+    private Transform origparent;
+
     //interface implementation for IBeginDragHandler
     public void OnBeginDrag(PointerEventData eventData)
     {
         //allow dragging (within the slot) if item exists
         if (item != null)
         {
-            this.transform.position = eventData.position;
+            origparent = this.transform.parent; //original parent is InventorySlot
+            this.transform.SetParent(this.transform.parent.parent); //set item parent to be InventorySlotsPanel (parent of InventorySlot)
+            this.transform.position = eventData.position; //set item position to cursor position
         }
     }
 
@@ -27,13 +31,13 @@ public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         //allow items to be dragged outside of slot
         if (item != null)
         {
-            this.transform.position = eventData.position;
+            this.transform.position = eventData.position; //set item position to cursor position
         }
     }
 
     //interface implementation for IEndDragHandler
     public void OnEndDrag(PointerEventData eventData)
     {
-        throw new NotImplementedException();
+        this.transform.SetParent(origparent); //set item parent to be slot again
     }
 }
