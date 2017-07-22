291f7c4c1568f04dc94de040cb6eca5a2f76a65a
Sam Wu <sjwu@ualberta.ca>
2017-07-21 22:10:03 -0700

    add tooltip script

---
+++ Sam's World/Assets/Scripts/ToolTipManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTipManager : MonoBehaviour {
+
+    private Item item;
+    private string itemname;
+    private GameObject tooltip;
+
+    void Start()
+    {
+        tooltip = GameObject.Find("ToolTip");
+    }
+
+    //show tooltip when mouse hovers over item
+    public void ToolTipOn(Item item)
+    {
+        this.item = item;
+    }
+
+    //turn off tooltip when mouse is not over an item
+    public void ToolTipOff()
+    {
+
+    }
+
+    //create string that contains tooltip information
+    //does text concaternation and styling
+    public void ConstructString()
+    {
+        itemname = item.Title;
+    }
+
+}
